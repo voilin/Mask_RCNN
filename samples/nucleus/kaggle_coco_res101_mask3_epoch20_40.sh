4978d5fe423144fcfad5017f46b70e473dd31b93
voilin <voilin@voilins-MacBook-Pro.local>
2018-04-12 01:40:37 +0800

    q

--- samples/nucleus/kaggle_coco_res101_mask3_epoch20_40.sh
+++ samples/nucleus/kaggle_coco_res101_mask3_epoch20_40.sh
@@ -2,4 +2,4 @@
 
 python3 nucleus_mask.py train --dataset=/home/voilin_itl/.kaggle/competitions/data-science-bowl-2018  --subset=stage1_train --weights=coco --mask_weight=3 --epoch_head=20 --epoch_all=40
 
-python3 nucleus_mask.py detect --dataset=/home/voilin_itl/.kaggle/competitions/data-science-bowl-2018 --subset=stage1_test --weights=last
+python3 nucleus.py detect --dataset=/home/voilin_itl/.kaggle/competitions/data-science-bowl-2018 --subset=stage1_test --weights=last
