28a0764f4d7bd959b443b36ad996257be7eb3e1d
voilin <voilin@voilins-MacBook-Pro.local>
2018-04-16 11:00:30 +0800

    q

--- samples/nucleus/nucleus.py
+++ samples/nucleus/nucleus.py
@@ -334,8 +334,8 @@ def rle_decode(rle, shape):
 def mask_to_rle(image_id, mask, scores):
     "Encodes instance masks to submission format."
     assert mask.ndim == 3, "Mask must be [H, W, count]"
-    print(mask)
-    print(scores)
+    if len(scores) == 0:
+        return ""
     # Remove mask overlaps
     # Multiply each instance mask by its score order
     # then take the maximum across the last dimension
